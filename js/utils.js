2f843a0d80d88d8b0b6758b90650135ebb796c86
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-07-31 07:02:06 +0000

    fix: update my zsh config Updated By Github Actions

--- js/utils.js
+++ js/utils.js
@@ -39,23 +39,60 @@ NexT.utils = {
     });
   },
 
-  /**
-   * One-click copy code support.
-   */
-  registerCopyCode: function() {
-    let figure = document.querySelectorAll('figure.highlight');
-    if (figure.length === 0) figure = document.querySelectorAll('pre:not(.mermaid)');
+  registerCodeblock: function(element) {
+    const inited = !!element;
+    let figure = (inited ? element : document).querySelectorAll('figure.highlight');
+    let isHljsWithWrap = true;
+    if (figure.length === 0) {
+      figure = document.querySelectorAll('pre:not(.mermaid)');
+      isHljsWithWrap = false;
+    }
     figure.forEach(element => {
-      element.querySelectorAll('.code .line span').forEach(span => {
-        span.classList.forEach(name => {
-          span.classList.replace(name, `hljs-${name}`);
+      if (!inited) {
+        let span = element.querySelectorAll('.code .line span');
+        if (span.length === 0) {
+          // Hljs without line_number and wrap
+          span = element.querySelectorAll('code.highlight span');
+        }
+        span.forEach(s => {
+          s.classList.forEach(name => {
+            s.classList.replace(name, `hljs-${name}`);
+          });
         });
-      });
-      if (!CONFIG.copycode.enable) return;
-      let target = element;
-      if (CONFIG.copycode.style !== 'mac') target = element.querySelector('.table-container') || element;
+      }
+      const height = parseInt(window.getComputedStyle(element).height.replace('px', ''), 10);
+      const needFold = CONFIG.fold.enable && (height > CONFIG.fold.height);
+      if (!needFold && !CONFIG.copycode.enable) return;
+      let target;
+      if (isHljsWithWrap && CONFIG.copycode.style === 'mac') {
+        target = element;
+      } else {
+        let box = element.querySelector('.code-container');
+        if (!box) {
+          // https://github.com/next-theme/hexo-theme-next/issues/98
+          // https://github.com/next-theme/hexo-theme-next/pull/508
+          const container = element.querySelector('.table-container') || element;
+          box = document.createElement('div');
+          box.className = 'code-container';
+          container.wrap(box);
+
+          // add "notranslate" to prevent Google Translate from translating it, which also completely messes up the layout
+          box.classList.add('notranslate');
+        }
+        target = box;
+      }
+      if (needFold && !target.classList.contains('unfold')) {
+        target.classList.add('highlight-fold');
+        target.insertAdjacentHTML('beforeend', '<div class="fold-cover"></div><div class="expand-btn"><i class="fa fa-angle-down fa-fw"></i></div>');
+        target.querySelector('.expand-btn').addEventListener('click', () => {
+          target.classList.remove('highlight-fold');
+          target.classList.add('unfold');
+        });
+      }
+      if (inited || !CONFIG.copycode.enable) return;
+      // One-click copy code support.
       target.insertAdjacentHTML('beforeend', '<div class="copy-btn"><i class="fa fa-copy fa-fw"></i></div>');
-      const button = element.querySelector('.copy-btn');
+      const button = target.querySelector('.copy-btn');
       button.addEventListener('click', () => {
         const lines = element.querySelector('.code') || element.querySelector('code');
         const code = lines.innerText;
@@ -376,6 +413,19 @@ NexT.utils = {
     panelContainer.style.setProperty('--active-panel-height', `${panelHeights[index]}px`);
 
     sidebar.classList.replace(activeClassNames[1 - index], activeClassNames[index]);
+  },
+
+  updateFooterPosition: function() {
+    if (CONFIG.scheme === 'Pisces' || CONFIG.scheme === 'Gemini') return;
+    function updateFooterPosition() {
+      const footer = document.querySelector('.footer');
+      const containerHeight = document.querySelector('.main').offsetHeight + footer.offsetHeight;
+      footer.classList.toggle('footer-fixed', containerHeight <= window.innerHeight);
+    }
+
+    updateFooterPosition();
+    window.addEventListener('resize', updateFooterPosition);
+    window.addEventListener('scroll', updateFooterPosition, { passive: true });
   },
 
   getScript: function(src, options = {}, legacyCondition) {
